8ed323215a0c497eb5b99533c7338951133de588
Luis Carlos Lozano <luisca0806@gmail.com>
2026-04-09 15:23:42 -0500

    acomodar  la UI

---
+++ Fuerzas_en_accion/Assets/Caso 1 Culumpio/Scripts/UIraycast.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIraycast : MonoBehaviour
+{
+    GameObject currentHover;
+    // Start is called before the first frame update
+    void Update()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        pointerData.position = new Vector2(Screen.width / 2, Screen.height / 2);
+
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        GameObject newHover = results.Count > 0 ? results[0].gameObject : null;
+
+        if (newHover != currentHover)
+        {
+            if (currentHover != null)
+                ExecuteEvents.Execute(currentHover, pointerData, ExecuteEvents.pointerExitHandler);
+
+            if (newHover != null)
+                ExecuteEvents.Execute(newHover, pointerData, ExecuteEvents.pointerEnterHandler);
+
+            currentHover = newHover;
+        }
+    }
+}
