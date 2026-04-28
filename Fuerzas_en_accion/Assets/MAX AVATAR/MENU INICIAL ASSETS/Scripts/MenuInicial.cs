01e58c7793b50d4a0c50924b19bf27a782a94984
JUANSUAREZ97 <juan_esteban.suarez@uao.edu.co>
2026-04-28 00:27:47 -0500

    BOTON_MENU_ANCLADO
    Se colocó el menú anclado en la parte superior izquierda en cada una de las escenas, este menu cuando se selecciona abre el menu normal para navegar alrededor de la experiencia o las otras escenas

--- Fuerzas_en_accion/Assets/MAX AVATAR/MENU INICIAL ASSETS/Scripts/MenuInicial.cs
+++ Fuerzas_en_accion/Assets/MAX AVATAR/MENU INICIAL ASSETS/Scripts/MenuInicial.cs
@@ -8,7 +8,7 @@ public class MenuInicial : MonoBehaviour
     // Start is called before the first frame update
     public void Jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("laboratorioInicial");
     }
 
     // Update is called once per frame
