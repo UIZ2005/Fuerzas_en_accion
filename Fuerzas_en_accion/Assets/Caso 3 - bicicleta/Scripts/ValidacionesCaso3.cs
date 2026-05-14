aab293208bca0fd1d06383bc823f75ffb16132d5
Luis Carlos Lozano <luisca0806@gmail.com>
2026-05-13 20:20:30 -0500

    HU 8.3.4 y 8.3.3

--- Fuerzas_en_accion/Assets/Caso 3 - bicicleta/Scripts/ValidacionesCaso3.cs
+++ Fuerzas_en_accion/Assets/Caso 3 - bicicleta/Scripts/ValidacionesCaso3.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ValidacionesCaso3 : MonoBehaviour
 {
     // Start is called before the first frame update
     private int pinonSelect;
     private int platoSelect;
+
+    public TMP_InputField plato;
+    public TMP_InputField pinon;
+
+    public GameObject[] preguntas;
+    private int i=0;
     void Start()
     {
         
@@ -27,7 +35,23 @@ public class ValidacionesCaso3 : MonoBehaviour
     {
         platoSelect = x;
     }
-    
+    public void validacionPlatoPinon()
+    {
+        if(plato.text== "16N*m")
+        {
+            plato.gameObject.GetComponent<Image>().color = Color.green;
+            if (pinon.text == "4N*m")
+            {
+                pinon.gameObject.GetComponent<Image>().color = Color.green;
+                SiguientePregunta();
+            }else{
+                pinon.gameObject.GetComponent<Image>().color = Color.red;
+            }
+        }
+        else{
+            plato.gameObject.GetComponent<Image>().color = Color.red;
+        }
+    }
     public void ValidacionPreguntaPlatoSelect()
     {
         if (pinonSelect ==1 && platoSelect==1)
@@ -42,7 +66,13 @@ public class ValidacionesCaso3 : MonoBehaviour
 
     public void SiguientePregunta()
     {
-
+        preguntas[i].SetActive(false);
+        i += 1;
+        if (preguntas[i] != null)
+        {
+            preguntas[i].SetActive(true);
+        }
+       
        //pone aqui lo de desactivar y activar los panels poneles id o que sea progresivo no se jsjsjsjsjs
     }
 }
